3580671bb3993e4dce5bb2462efb917103513ea3
Marcos Santos <marcos.goncalvesdossantos@gmail.com>
2018-12-15 22:46:16 -0200

    Produto e associação muitos para muitos

--- src/main/java/com/md/cursoSpring/CursoSpringApplication.java
+++ src/main/java/com/md/cursoSpring/CursoSpringApplication.java
@@ -8,14 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.md.cursoSpring.domain.Categoria;
+import com.md.cursoSpring.domain.Produto;
 import com.md.cursoSpring.repositories.CategoriaRepository;
+import com.md.cursoSpring.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class CursoSpringApplication implements CommandLineRunner{
-	//Command Line Runner server para realizar açoes no load do Spring
-	
+public class CursoSpringApplication implements CommandLineRunner {
+	// Command Line Runner server para realizar açoes no load do Spring
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
@@ -25,8 +30,23 @@ public class CursoSpringApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto p1 = new Produto(null, "Computador", 2000);
+		Produto p2 = new Produto(null, "Impressora", 800);
+		Produto p3 = new Produto(null, "Mouse", 80);
+
+		//cat1.setProdutos(Arrays.asList(p1, p2, p3));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		cat2.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+
 	}
 }
