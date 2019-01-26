d029b4decf1317f46eff88d82e3f571fd15d761f
Marcos Santos <marcos.goncalvesdossantos@gmail.com>
2019-01-25 22:04:40 -0200

    Inserindo uma nova Categoria com Post

--- src/main/java/com/md/cursoSpring/resources/CategoriaResource.java
+++ src/main/java/com/md/cursoSpring/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.md.cursoSpring.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.md.cursoSpring.domain.Categoria;
 import com.md.cursoSpring.services.CategoriaService;
@@ -13,14 +17,24 @@ import com.md.cursoSpring.services.CategoriaService;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService service;
-	
-	@RequestMapping(value="/{id}",method = RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
-		
-		return ResponseEntity.ok().body(obj); //Retorna uma Entidade de Resposta com uma mensagem ok e o corpo é o resultado da busca
+
+		return ResponseEntity.ok().body(obj); // Retorna uma Entidade de Resposta com uma mensagem ok e o corpo é o
+												// resultado da busca
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build(); // O método created retorna http 201
 	}
 }
