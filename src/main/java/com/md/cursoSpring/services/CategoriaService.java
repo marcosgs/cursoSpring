e090146098bc97ca948b6f718f10fb573e45c114
Marcos Santos <marcos.goncalvesdossantos@gmail.com>
2019-01-27 20:09:26 -0200

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/md/cursoSpring/services/CategoriaService.java
+++ src/main/java/com/md/cursoSpring/services/CategoriaService.java
@@ -15,7 +15,7 @@ public class CategoriaService {
 	@Autowired // Declara as dependencias e instancia elas no Spring
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -23,9 +23,13 @@ public class CategoriaService {
 
 	}
 
-	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		this.find(obj.getId());
 		return repo.save(obj);
 	}
 }
