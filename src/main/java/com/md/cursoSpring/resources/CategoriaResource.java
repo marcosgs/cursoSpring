04292a8a610c14e3a484b860ca2f9974e1326ba2
Marcos <marcos.goncalvesdossantos@gmail.com>
2019-03-24 19:53:53 -0300

    Paginação com parametros opcionais na requisicao

--- src/main/java/com/md/cursoSpring/resources/CategoriaResource.java
+++ src/main/java/com/md/cursoSpring/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -61,12 +63,28 @@ public class CategoriaResource {
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 
 		List<Categoria> list = service.findAll();
-		
-		//Utiliza o stream e o map para varrer a lista e atribuir os valores ao CategoriaDTO
+
+		// Utiliza o stream e o map para varrer a lista e atribuir os valores ao
+		// CategoriaDTO
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 
 		return ResponseEntity.ok().body(listDTO); // Retorna uma Entidade de Resposta com uma mensagem ok e o corpo é o
-												// resultado da busca
+													// resultado da busca
+	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> fingPage(
+			@RequestParam(name = "page", defaultValue = "0") Integer page,
+			@RequestParam(name = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(name = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(name = "direction", defaultValue = "ASC") String direction) {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+
+		// Utiliza o stream e o map para varrer a lista e atribuir os valores ao
+		// CategoriaDTO
+		Page<CategoriaDTO> listDTO = list.map(obj -> new CategoriaDTO(obj));
+
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
